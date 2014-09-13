edd14009fe119986271af3531e9cb19a2f3b9929
agurtovoy <agurtovoy@acm.org>
2014-09-13 03:27:50 -0500

    More redirects for the initial press release

--- server/index.js
+++ server/index.js
@@ -45,8 +45,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get( '/', routes.page.bind( app, 'index' ) );
+//app.get( '/', routes.page.bind( app, 'index' ) );
+app.get( '/', function( req, res ) { res.redirect( 'https://medium.com/@ICPDwatch/press-release-witness-of-the-9-6-14-police-raid-comes-forward-with-testimony-1cba6f844e52' ); } );
 app.get( '/ICPD-party-bust-2014-09-06', function( req, res ) { res.redirect( 'https://medium.com/@VoteNoNewJail/icpd-party-bust-7b2516208cde' ); } );
 app.get( '/no-justice-no-peace', function( req, res ) { res.redirect( 'https://medium.com/@VoteNoNewJail/re-icpd-party-bust-d55d89530c00' ); } );
+app.get( '/2014-09-06-raid-testimony', function( req, res ) { res.redirect( 'https://www.youtube.com/watch?v=G5-te39yvYY' ); } );
 
 exports.app = app;
